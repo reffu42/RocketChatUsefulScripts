c9de252591a93b9a0908cc7c843b5b1c12d7f636
reffu42 <1823982+reffu42@users.noreply.github.com>
2019-01-09 14:47:51 -0500

    Update rc-emoji-prepend.user.js

--- rc-emoji-prepend.user.js
+++ rc-emoji-prepend.user.js
@@ -4,32 +4,31 @@
 // @version      1.2
 // @description  Script to prepend text to the beginning of every message (most likely an emoji)
 // @author       reffu42
-// @match        
+// @match
 // @grant        GM_addStyle
 // ==/UserScript==
 
 
 (function() {
-    'use strict';    
-    var EMOJI = '';
-      
-    
+    'use strict';
+    const EMOJI = ':beret:';
+
     function setUp() {
         var footers = $('aside footer');
         if(footers.length === 0) {
             setTimeout(setUp, 1000);
             return;
         }
-        footers.append('<label style="color:lightgray !important;">Emoji Prepend <input type="checkbox" style="vertical-align:middle;" id="prepend-emoji" checked/></label>');            
-        ChatMessages.prototype.oldSend = ChatMessages.prototype.send;        
+        footers.append('<label style="color:lightgray !important;">Emoji Prepend <input type="checkbox" style="vertical-align:middle;" id="prepend-emoji" checked/></label>');
+        ChatMessages.prototype.oldSend = ChatMessages.prototype.send;
         ChatMessages.prototype.send = function(roomId, textArea) {
             var special = $('#prepend-emoji').prop('checked');
-            if(special) {
+            if(special && !roomObj.editing.id) {
               textArea.value = EMOJI + " " + textArea.value;
             }
             var roomObj = chatMessages[roomId];
             roomObj.oldSend(roomId, textArea);
-        };      
+        };
     }
     setUp();
 
